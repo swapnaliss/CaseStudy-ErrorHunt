72595a9b083987d57f4d2e7418a64db03e4f2558
swapnaliss <swapnalisbhargude123@gmail.com>
2022-01-02 10:48:46 +0530

    Part 2 point7

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = new express;
 app.set('views','./src/views'); 
 app.set('view engine','ejs'); 
 
-
+app.use(cors()); //Part #2 Point 7
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname , '/public'))); 
