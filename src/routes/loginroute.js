4d2a2b48096c146004b4bde2502029ecbe0e8bc6
swapnaliss <swapnalisbhargude123@gmail.com>
2022-01-02 11:03:00 +0530

    Part 2 Point 10

--- src/routes/loginroute.js
+++ src/routes/loginroute.js
@@ -11,8 +11,8 @@ loginRouter.get('/',function(req,res){
 
 loginRouter.get("/check",function(req,res){
     var checkuser = {
-        uid:req.param("uid"),
-        pwd:req.param("pwd")
+        uid:req.query.uid,
+        pwd:req.query.pwd //Part #2 , Point 10
     };
     
     console.log(checkuser);
